124a8ccf558d9ad0d129ec6831fa0516a87ded9f
Edson P. Miranda Callo <83046821+EdsonMC29@users.noreply.github.com>
2023-10-04 01:14:16 -0500

    Seeking, Wandering y automata

--- ProgramBot/Assets/Scripts/MoveToPlayer.cs
+++ ProgramBot/Assets/Scripts/MoveToPlayer.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class MoveToPlayer : MonoBehaviour {
     public float speed = 2.0f;
-    public float accuracy = 0.0f;
+    public float accuracy = 0.01f;
     public Transform player;
     void Start() {
-
+        this.transform.LookAt(player.position);
     }
 
-    void Update() {
-        Vector3 direction = (player.position - this.transform.position) * 0.5f;
-        if(direction.magnitude < accuracy)
-            this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+    void LateUpdate() {
+        Vector3 direction = player.position - this.transform.position;
+        if(direction.magnitude > 0.2f)
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
     }
 }
--- ProgramBot/Assets/Scripts/Player_controler.cs
+++ ProgramBot/Assets/Scripts/Player_controler.cs
@@ -2,30 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_controler : MonoBehaviour
-{
 
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Player_Controler : MonoBehaviour {
+ 
+    [Header("Variables Movimiento Del Personaje")]
+    public CharacterController controlador;
+    //public Transform camara;
+    public float velocidadDeMovimiento;
+    private float rotacionSuave = 0.1f;
+    float velocidadRotacionSuave;
 
+
+    [Header("Variables Salto y Suelo")]
+
+    public Transform chequeadorDeSuelo;
+    public float distanciaAlSuelo;
+    public LayerMask mascaraDeSuelo;
+
+    Vector3 velocidad;
+    bool estaEnElSuelo;
+    public float gravedad = -9.81f;
+    public float alturaDelSalto;
+
+    //Animacion
+    [Header("Variables de Animación")]
+    public Animator animator;
+
+
+    public string variableMovimiento;
+    public string variableSuelo;
+
+
+
+    void Start(){
+        controlador = GetComponent<CharacterController>();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
 
-       
+    void Update(){
+
+        estaEnElSuelo = Physics.CheckSphere(chequeadorDeSuelo.position, distanciaAlSuelo, mascaraDeSuelo);
+        if(estaEnElSuelo && velocidad.y <0){
+            velocidad.y =-2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direccion = new Vector3(horizontal,0f,vertical).normalized; //Normalized es para que al moverse en diagonal no vaya más rápido
+        animator.SetFloat(variableMovimiento, (Mathf.Abs(vertical) + Mathf.Abs(horizontal)));
+
+        if(direccion.magnitude >= 0.1f){
+            float anguloARotar = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg;
+            float angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloARotar, ref velocidadRotacionSuave, rotacionSuave);
+            transform.rotation = Quaternion.Euler(0f,angulo,0f);
+
+            Vector3 direccionDelMovimiento = Quaternion.Euler(0f, anguloARotar, 0f) * Vector3.forward;
+            controlador.Move(direccionDelMovimiento.normalized * velocidadDeMovimiento * Time.deltaTime);
+        }
+
+        if(Input.GetButtonDown("Jump") && estaEnElSuelo){
+                velocidad.y = Mathf.Sqrt(alturaDelSalto *-2f * gravedad);
+        }
+
+        velocidad.y += gravedad * Time.deltaTime;
+        controlador.Move(velocidad * Time.deltaTime);
+
+        animator.SetBool(variableSuelo, controlador.isGrounded);
+    }
     
-        
-        
-    }
-
-    private void FixedUpdate() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movimiento = new Vector3(horizontalInput,0,verticalInput);
-        transform.position = transform.position + movimiento * 6.0f * Time.deltaTime;
-   
-    }
 }
